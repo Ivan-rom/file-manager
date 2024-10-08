82db1d7d7f600e398f4c09f0dd672e6d7e301fea
Ivan-rom <rom_1van@icloud.com>
2024-10-08 22:13:35 +0600

    feat: implement os info module

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import { homedir } from "node:os";
 
 import { changeDirectory, logDirectory } from "./nwd.js";
 import { greeting, close } from "./prompts.js";
+import { osInfo } from "./os.js";
 
 greeting();
 changeDirectory(homedir());
 
-process.stdin.on("data", (data) => {
+process.stdin.on("data", async (data) => {
   const dataString = data.toString().trim();
 
   const command = dataString.split(" ")[0];
@@ -27,6 +28,11 @@ process.stdin.on("data", (data) => {
     case "cd":
       const newDirectory = dataString.split(" ").slice(1).join(" ").trim();
       changeDirectory(newDirectory);
+      break;
+
+    case "os":
+      const option = dataString.split(" ").slice(1)[0];
+      osInfo(option);
       break;
 
     default:
