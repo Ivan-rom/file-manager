9ba64b4a8a4fb4b81b5ddd0d86b312b1a6c114dd
Ivan Romanenko <rom_1van@icloud.com>
2025-10-29 14:47:42 +0600

    feat: implement nwd

--- src/app.js
+++ src/app.js
@@ -1,20 +1,85 @@
 import process from "node:process";
+import os from "node:os";
+import fs from "node:fs";
 
-const USERNAME =
-  process.argv
-    .slice(2)
-    .find((arg) => arg.toLowerCase().startsWith("--username"))
-    ?.split("=")[1] || "guest";
+function initializeApp() {
+  const USERNAME = parseUsername();
 
-console.log(`Welcome to the File Manager, ${USERNAME}!`);
+  process.chdir(os.homedir());
 
-process.stdin.on("data", (buffer) => {
-  const data = buffer.toString().trim();
+  console.log(`Welcome to the File Manager, ${USERNAME}!`);
+  logDir();
+  console.log("Please input a command");
 
-  if (data === ".exit") process.exit();
-});
+  process.stdin.on("data", (buffer) => {
+    const data = buffer.toString().trim();
+    const [command, ...args] = data.split(" ");
 
-process.on("SIGINT", () => process.exit());
-process.on("exit", () => {
-  console.log(`Thank you for using File Manager, ${USERNAME}, goodbye!`);
-});
+    try {
+      switch (command) {
+        case ".exit":
+          process.exit();
+
+        case "up":
+          process.chdir("..");
+          logDir();
+          break;
+
+        case "cd":
+          const rawPath = args.join(" ");
+          const path =
+            rawPath.startsWith("/") || rawPath.startsWith("\\")
+              ? `${process.cwd()}/${rawPath}`
+              : rawPath;
+
+          process.chdir(path);
+          logDir();
+          break;
+
+        case "ls":
+          fs.readdir(process.cwd(), { withFileTypes: true }, (err, dirent) => {
+            if (err) throw err;
+
+            console.table(
+              dirent.map((d) => ({
+                name: d.name,
+                type: getType(d),
+              }))
+            );
+          });
+
+        default:
+          console.log("Invalid input");
+          break;
+      }
+    } catch {
+      console.log("Operation failed");
+    }
+  });
+
+  process.on("SIGINT", () => process.exit());
+  process.on("exit", () => {
+    console.log(`Thank you for using File Manager, ${USERNAME}, goodbye!`);
+  });
+}
+
+function getType(dirent) {
+  if (dirent.isFile()) return "file";
+  if (dirent.isDirectory()) return "directory";
+  return "other";
+}
+
+function parseUsername() {
+  return (
+    process.argv
+      .slice(2)
+      .find((arg) => arg.toLowerCase().startsWith("--username"))
+      ?.split("=")[1] || "guest"
+  );
+}
+
+function logDir() {
+  console.log(`You are currently in: ${process.cwd()}`);
+}
+
+initializeApp();
