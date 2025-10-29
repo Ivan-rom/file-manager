a79bf3efd6c77a739fd02719ecb19080ac910219
Ivan Romanenko <rom_1van@icloud.com>
2025-10-29 21:50:10 +0600

    fix: move hash module

--- src/dispatch.js
+++ src/dispatch.js
@@ -3,7 +3,7 @@ import logDirectory from "./helpers/logDirectory.js";
 import navigation from "./navigation/index.js";
 import filesOperations from "./filesOperations/index.js";
 import operatingSystem from "./operatingSystem/index.js";
-import hashCalculate from "../hashCalculate/index.js";
+import hashCalculate from "./hashCalculate/index.js";
 
 export default async function dispatch(data) {
   try {
